8f433bb1261fa64648b9a2467727b05449758af5
Vladislav Rykovskiy <v.rykovskiy@volka.com>
2024-07-05 18:29:31 +0300

    read page number on about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,14 +1,34 @@
 import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, getRouteApi, useRouter, useSearch } from '@tanstack/react-router'
+
+import { z } from 'zod'
+
+const messageSearchSchema = z.object({
+    page: z.number().catch(1),
+})
 
 export const Route = createFileRoute('/about')({
-  component: AboutComponent,
+    component: AboutComponent,
+    validateSearch: messageSearchSchema,
+    onStay: (match) => {
+        console.log('onStay page number', match.search.page)
+    },
 })
 
 function AboutComponent() {
-  return (
-    <div className="p-2">
-      <h3>About</h3>
-    </div>
-  )
+    const router = useRouter()
+    const routeApi = getRouteApi('/about')
+    const { page } = routeApi.useSearch()
+
+    const handleButtonClick = (pageNumber: number) => {
+        router.navigate({ search: { page: pageNumber }, replace: true })
+    }
+
+    return (
+        <div className="p-2">
+            <h3>About</h3>
+            <p>Current page: {page}</p>
+            <button onClick={() => handleButtonClick(page + 1)}>Go to next page</button>
+        </div>
+    )
 }
